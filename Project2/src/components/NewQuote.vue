4cca0bcf9c31bc76d53805eb7e948ff2e5ce6a72
Vic <ab889721@gmail.com>
2018-08-06 22:40:22 +0800

    Project 2 done

---
+++ Project2/src/components/NewQuote.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="row">
+        <form action="">
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <label for="">Quote</label>
+                <textarea v-model="quote" name="" id="" cols="30" rows="3" class="form-control"></textarea>
+            </div>
+                
+
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <button class="btn btn-primary" @click.prevent="createNew" :disabled="!quote">Add Quote</button>
+            </div>
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            quote: ''
+        }
+    },
+    methods: {
+        createNew() {
+            this.$emit('quoteAdded', this.quote)
+            this.quote = ''
+        }
+    }
+}
+</script>
+<style>
+</style>
---
+++ Project2/src/components/QuoteGrid.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="row">
+        <!-- 這邊用 @click.native 為的是要讓其子組件能被監聽到,
+         Vue預設的 click 只會監聽到其組件，不像原生的DOM能夠監聽到子元素-->
+        <app-quote v-for="(quote,index) in quotes" :key="index" @click.native="deleteQuote(index)">{{quote}}</app-quote>
+    </div>
+</template>
+<script>
+import Quote from './Quote.vue'
+export default {
+    props: ['quotes'],
+    methods: {
+        deleteQuote(index) {
+            this.$emit('deleteQuote', index)
+        }
+    },
+    components: {
+        appQuote: Quote
+    }
+}
+</script>
+
+<style>
+</style>
